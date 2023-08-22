11bd38383e53b39eaf723736b2a6de52c4bfffe3
odiegofarias <diego.farias.fernandes@gmail.com>
2023-08-22 19:42:51 -0300

    Criada funcionalidade para seguir usuário

--- msg/views.py
+++ msg/views.py
@@ -20,6 +20,21 @@ def profile(request, pk):
     if request.user.is_authenticated:
         profile = Profile.objects.get(user_id=pk)
 
+        # Post form Logic
+        if request.method == "POST":
+            # Get current user ID
+            current_user_profile = request.user.profile
+            # Get form date
+            action = request.POST['follow']
+            # Decide to follow or unfollow
+            if action == "unfollow":
+                current_user_profile.follows.remove(profile)
+            elif action == "follow":
+                current_user_profile.follows.add(profile)
+
+            # Save the profile
+            current_user_profile.save()
+
         return render(request, 'profile.html', {'profile': profile})
 
     else:
